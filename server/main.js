5781325d706a48f4967bfeafb4982380e3e65cf5
Cody <icodyfong@gmail.com>
2023-01-02 20:38:43 -0700

    fixed server

--- server/main.js
+++ server/main.js
@@ -8,4 +8,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static(`public`))
 
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
+})
+
 app.listen(4000, console.log('Server running on 4000'))
